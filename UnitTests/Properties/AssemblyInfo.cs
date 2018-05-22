d68560e3ab0e240ade4d9fa448ce17d6df3b4092
Zoltanar <universegusty@gmail.com>
2018-05-22 22:28:33 +0100

    Added initial tests.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("440b7579-33de-4d10-8224-7036a6a6d4d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AcuCafe;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class UnitTests
+	{
+		[TestMethod]
+		public void MakeIceTeaWithMilkSugar()
+		{
+			throw new NotImplementedException();
+		}
+
+		[TestMethod]
+		public void MakeEspressoWithMilkSugar()
+		{
+			throw new NotImplementedException();
+		}
+		
+		[TestMethod]
+		public void MakeEspressoWithChocolate()
+		{
+			throw new NotImplementedException();
+		}
+
+		[TestMethod]
+		public void StopMilkInIceTea()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
